5bcf1fc0f40abf398ecedc41b89701a2472eae7c
WD <1105848296@qq.com>
2025-01-28 00:33:47 +0800

    lsp: add debug option in logger

--- src/annotation_lsp/logger.py
+++ src/annotation_lsp/logger.py
@@ -5,16 +5,17 @@ from lsprotocol import types
 class Logger:
 	def __init__(self):
 		self._server = None
+		self.debug = True
 
 	def set_server(self, server):
 		self._server = server
 
 	def error(self, msg: str) -> None:
-		if self._server:
+		if self._server and self.debug:
 			self._server.show_message(msg, types.MessageType.Error)
 
 	def info(self, msg: str) -> None:
-		if self._server:
+		if self._server and self.debug:
 			self._server.show_message(msg, types.MessageType.Info)
 
 logger = Logger()
